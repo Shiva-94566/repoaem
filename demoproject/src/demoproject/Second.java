ba41f166739056cb383749a0a5ae4554ee462cca
612833744 <shivateotia001@gmail.com>
2019-09-10 14:42:50 +0530

    hye mssg

--- demoproject/src/demoproject/Second.java
+++ demoproject/src/demoproject/Second.java
@@ -1,9 +1,9 @@
 package demoproject;
 
-public class First {
+public class Second {
 
 	public static void main(String[] args) {
-		System.out.print("hye first");
+		System.out.print("hye second");
 	}
 
 }
